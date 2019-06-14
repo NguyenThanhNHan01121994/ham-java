23d9662955d306af2cdc2515f5636c01241c47c8
NguyenThanhNHan01121994 <50906464+NguyenThanhNHan01121994@users.noreply.github.com>
2019-06-14 09:49:08 +0700

    Add files via upload

---
+++ hinhtamgiac.java
@@ -0,0 +1,26 @@
+package hinhtamgiac;
+import java.util.Scanner;
+
+public class hinhtamgiac {
+		public static void main(String[] args) {
+			int num, n;
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Nhap n: ");
+			num = sc.nextInt();
+			System.out.println();
+			n = num;
+			for (int r = 1; r <= num; r++) {
+				for (int c = num - r; c >= 1; c--) {
+					System.out.print("*");
+				}
+				for (int c = 1; c <= n; c++) {
+					System.out.print("*");
+				}
+				for (int sp = 1; sp <= r; sp++) {
+					System.out.print(" ");
+				}
+				n--;
+				System.out.println();
+			}
+		}
+}
---
+++ random.java
@@ -0,0 +1,16 @@
+package random;
+import java.util.Random;
+
+public class random {
+
+public static void main(String[] args) {
+    Random rd = new Random();  
+    int number = rd.nextInt(); 
+    System.out.println("So vua duoc sinh ra la " + number);
+         
+    int number1 = rd.nextInt(4); 
+    System.out.println("So vua duoc sinh ra la " + number1);
+    int number2 = -4 + rd.nextInt(4);   
+    System.out.println("So vua duoc sinh ra la " + number2);	
+}
+}
